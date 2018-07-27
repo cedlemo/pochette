a2eab152da6234bb8764fbefa935f696e0735c8f
cedlemo <cedlemo@gmx.com>
2018-07-27 18:45:53 +0200

    Define the root of the REST path

---
+++ src/fr/pochette/rest/ConfigurationApplicationREST.java
@@ -0,0 +1,9 @@
+package fr.pochette.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class ConfigurationApplicationREST extends Application {
+
+}
